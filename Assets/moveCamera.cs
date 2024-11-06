2bc75d3196f4c1b3331fa36f9c9e0c969c87a044
KaushalBinjola <81644890+KaushalBinjola@users.noreply.github.com>
2024-11-06 00:47:42 -0800

    level 5

--- Assets/moveCamera.cs
+++ Assets/moveCamera.cs
@@ -7,13 +7,18 @@ public class MoveCamera : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player;
+    public int maxX;
+    public int maxY;
+
+    public int offsetY = 0;
+    public int offsetX = 0;
 
     // Update is called once per frame
     void Update()
     {
         // move the camera in a boxed region from the center of the screen
-        float camX = math.abs(player.transform.position.x) / (player.transform.position.x) * math.min(1, math.abs(player.transform.position.x));
-        float camY = math.abs(player.transform.position.y) / (player.transform.position.y) * math.min(1, math.abs(player.transform.position.y));
+        float camX = math.abs(player.transform.position.x) / (player.transform.position.x) * math.min(maxX, math.abs(player.transform.position.x)) + offsetX;
+        float camY = math.abs(player.transform.position.y) / (player.transform.position.y) * math.min(maxY, math.abs(player.transform.position.y)) + offsetY;
         transform.position = new Vector3(camX, camY, transform.position.z);
     }
 }
